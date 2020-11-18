8e36181dce007f7bf668a14d8db2d7f50862a6c7
brian926 <brian.antunes@yahoo.com>
2020-11-18 07:37:10 -0500

    Added scouting and visual inputs

--- pythonsc.py
+++ pythonsc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import sc2
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, position
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
-CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
+CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, OBSERVER, ROBOTICSFACILITY
 import random
 import cv2
 import numpy as np
@@ -19,6 +19,7 @@ class SentdeBot(sc2.BotAI):
 
 		# What to do every step
 		# In SCAI/bot_ai.py
+		await self.scout()
 		await self.distribute_workers()
 		await self.build_workers()
 		await self.build_pylons()
@@ -29,14 +30,83 @@ class SentdeBot(sc2.BotAI):
 		await self.intel()
 		await self.attack()
 
+	def random_location_variance(self, enemy_start_location):
+		x = enemy_start_location[0]
+		y = enemy_start_location[1]
+
+		x += ((random.randrange(-20, 20))/100) * enemy_start_location[0]
+		y += ((random.randrange(-20, 20))/100) * enemy_start_location[1]
+
+		if x < 0:
+			x = 0
+		if y < 0:
+			y = 0
+		if x > self.game_info.map_size[0]:
+			x = self.game_info.map_size[0]
+		if y > self.game_info.map_size[1]:
+			y = self.game_info.map_size[1]
+		
+		go_to = position.Point2(position.Pointlike((x,y)))
+		return go_to
+
+	async def scout(self):
+		if len(self.units(OBSERVER)) > 0:
+			scout = self.units(OBSERVER)[0]
+			if scout.is_idle:
+				enemy_location = self.enemy_start_locations[0]
+				move_to = self.random_location_variance(enemy_location)
+				print(move_to)
+				await self.do(scout.move(move_to))
+		else:
+			for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
+				if self.can_afford(OBSERVER) and self.supply_left > 0:
+					await self.do(rf.train(OBSERVER))
+
 	async def intel(self):
-		print(self.game_info.map_size)
-		# Flip around, it's y, x when dealing with an array
 		game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0], 3), np.uint8)
-		for nexus in self.units(NEXUS):
-			nex_pos = nexus.position
-			print(nex_pos)
-			cv2.circle(game_data, (int(nex_pos[0]), int(nex_pos[1])), 10, (0, 255, 0), -1)
+
+		draw_dict ={
+			NEXUS: [15, (0, 255, 0)],
+			PYLON: [3, (20,2235, 0)],
+			ASSIMILATOR: [1, (55, 200, 0)],
+			GATEWAY: [3, (200, 100, 0)],
+			CYBERNETICSCORE: [3, (150, 150, 0)],
+			STARGATE: [5, (255, 0, 0)],
+			VOIDRAY: [3, (255, 199, 0)],
+			ROBOTICSFACILITY: [5, (215, 155, 0)],
+		}
+
+		for unit_type in draw_dict:
+			for unit in self.units(unit_type).ready:
+				pos = unit.position
+				cv2.circle(game_data, (int(pos[0]), int(pos[1])), draw_dict[unit_type][0], draw_dict[unit_type][1], -1)
+
+		main_base_names = ["nexus", "commandcenter", "hatchery"]
+		for enemy_building in self.known_enemy_structures:
+			pos = enemy_building.position
+			if enemy_building.name.lower() not in main_base_names:
+				cv2.circle(game_data, (int(pos[0]), int(pos[1])), 5, (200, 50, 212), -1)
+		for enemy_building in self.known_enemy_structures:
+			pos = enemy_building.position
+			if enemy_building.name.lower() in main_base_names:
+				cv2.circle(game_data, (int(pos[0]), int(pos[1])), 15, (0, 0, 255), -1)
+
+		for enemy_unit in self.known_enemy_units:
+
+			if not enemy_unit.is_structure:
+				work_names = ["probe",
+								"scv",
+								"drone"]
+				# If that unit is a PROBE, SCV, or DRONE then it's a worker
+				pos = enemy_unit.position
+				if enemy_unit.name.lower() in work_names:
+					cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (55, 0, 155), -1)
+				else:
+					cv2.circle(game_data, (int(pos[0]), int(pos[1])), 3, (50, 0, 215), -1)
+
+		for obs in self.units(OBSERVER).ready:
+			pos = obs.position
+			cv2.circle(game_data, (int(pos[0]), int(pos[1])), 1, (255, 255, 255), -1)
 
 		# Flip horizontally to make our final fix in visual representation
 		flipped = cv2.flip(game_data, 0)
@@ -88,6 +158,11 @@ class SentdeBot(sc2.BotAI):
 					await self.build(GATEWAY, near = pylon)
 
 			if self.units(CYBERNETICSCORE).ready.exists:
+				if len(self.units(ROBOTICSFACILITY)) < 1:
+					if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
+						await self.build(ROBOTICSFACILITY, near=pylon)
+
+			if self.units(CYBERNETICSCORE).ready.exists:
 				if len(self.units(STARGATE)) < (self.iteration / self.ITERATIONS_PER_MINUTE):
 					if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
 						await self.build(STARGATE, near = pylon)
@@ -106,14 +181,15 @@ class SentdeBot(sc2.BotAI):
 			return self.enemy_start_locations[0]
 
 	async def attack(self):
-		aggressive_units = {
-							#STALKER: [15, 5],
-							VOIDRAY: [8, 3]
-							}
-		
+		aggressive_units = {VOIDRAY: [8, 3]}
 		for UNIT in aggressive_units:
-			for s in self.units(UNIT).idle:
-				await self.do(s.attack(self.find_target(self.state)))
+			if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+				for s in self.units(UNIT).idle:
+					await self.do(s.attack(self.find_target(self.state)))
+			elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+				if len(self.known_enemy_units) > 0:
+					for s in self.units(UNIT).idle:
+						await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 
